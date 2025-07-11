713c304f45271855435dce236e335e94271c3334
Christian Mortensen <chrimo@kemp-lauritzen.dk>
2025-07-11 10:58:38 +0200

    getting create server to work

--- PteroSharp.NET/Objects/V1_0/Client/CreateServerRequest.cs
+++ PteroSharp.NET/Objects/V1_0/Client/CreateServerRequest.cs
@@ -11,192 +11,260 @@ namespace PteroSharp.NET.Objects.V1_0.Client
 {
     public class CreateServerRequest
     {
-        [Required]
         [JsonPropertyName("name")]
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; }
 
         [JsonPropertyName("description")]
-        public string? Description { get; set; }
+        public string Description { get; set; }
 
-        [Required]
         [JsonPropertyName("user")]
         public int User { get; set; }
 
-        [Required]
         [JsonPropertyName("egg")]
         public int Egg { get; set; }
 
-        [Required]
         [JsonPropertyName("docker_image")]
-        public string docker_image { get; set; } = string.Empty;
+        public string docker_image { get; set; }
 
-        [Required]
         [JsonPropertyName("startup")]
-        public string Startup { get; set; } = string.Empty;
+        public string Startup { get; set; }
 
-        [Required]
-        [JsonPropertyName("environment")]
-        public Environment Environment { get; set; } = new();
-
-        [Required]
         [JsonPropertyName("limits")]
-        public ServerLimits Limits { get; set; } = new();
+        public ServerResourceLimits Limits { get; set; }
 
         [JsonPropertyName("feature_limits")]
-        public FeatureLimits? feature_limits { get; set; }
+        public ServerFeatureLimits feature_limits { get; set; }
 
-        [Required]
         [JsonPropertyName("allocation")]
-        public AllocationConfig Allocation { get; set; } = new();
+        public AllocationConfig Allocation { get; set; }
 
-        [JsonPropertyName("deploy")]
-        public DeployConfig? Deploy { get; set; }
-
-        [JsonPropertyName("start_on_completion")]
-        public bool StartOnCompletion { get; set; } = true;
-
-        [JsonPropertyName("skip_scripts")]
-        public bool SkipScripts { get; set; } = false;
-
-        [JsonPropertyName("oom_disabled")]
-        public bool OomDisabled { get; set; } = false;
+        [JsonPropertyName("environment")]
+        public Dictionary<string, string> Environment { get; set; }
     }
 
-    public class Environment{
-        public string server_jarfile { get; set; }
-        public string build_number { get; set; }
-    }
-    public class ServerLimits
+    public class ServerResourceLimits
     {
-        [Required]
         [JsonPropertyName("memory")]
         public int Memory { get; set; }
 
         [JsonPropertyName("swap")]
-        public int Swap { get; set; } = 0;
+        public int Swap { get; set; }
 
-        [Required]
         [JsonPropertyName("disk")]
         public int Disk { get; set; }
 
         [JsonPropertyName("io")]
-        public int Io { get; set; } = 500;
+        public int Io { get; set; }
 
         [JsonPropertyName("cpu")]
-        public int Cpu { get; set; } = 100;
+        public int Cpu { get; set; }
     }
 
-    public class FeatureLimits
+    public class ServerFeatureLimits
     {
         [JsonPropertyName("databases")]
-        public int? Databases { get; set; }
-
-        [JsonPropertyName("allocations")]
-        public int? Allocations { get; set; }
+        public int Databases { get; set; }
 
         [JsonPropertyName("backups")]
-        public int? Backups { get; set; }
+        public int Backups { get; set; }
+
+        [JsonPropertyName("allocations")]
+        public int Allocations { get; set; }
     }
 
     public class AllocationConfig
     {
-        [Required]
         [JsonPropertyName("default")]
         public int Default { get; set; }
     }
+    //public class CreateServerRequest
+    //{
+    //    [Required]
+    //    [JsonPropertyName("name")]
+    //    public string Name { get; set; } = string.Empty;
 
-    public class DeployConfig
-    {
-        [JsonPropertyName("locations")]
-        public List<int>? Locations { get; set; }
+    //    [JsonPropertyName("description")]
+    //    public string? Description { get; set; }
 
-        [JsonPropertyName("dedicated_ip")]
-        public bool DedicatedIp { get; set; } = false;
+    //    [Required]
+    //    [JsonPropertyName("user")]
+    //    public int User { get; set; }
 
-        [JsonPropertyName("port_range")]
-        public List<string>? PortRange { get; set; }
-    }
+    //    [Required]
+    //    [JsonPropertyName("egg")]
+    //    public int Egg { get; set; }
 
-    // Response model for created server
-    public class ServerResponse
-    {
-        [JsonPropertyName("object")]
-        public string Object { get; set; } = string.Empty;
+    //    [Required]
+    //    [JsonPropertyName("docker_image")]
+    //    public string docker_image { get; set; } = string.Empty;
 
-        [JsonPropertyName("attributes")]
-        public ServerAttributes Attributes { get; set; } = new();
-    }
+    //    [Required]
+    //    [JsonPropertyName("startup")]
+    //    public string Startup { get; set; } = string.Empty;
 
-    public class ServerAttributes
-    {
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
+    //    [Required]
+    //    [JsonPropertyName("environment")]
+    //    public Environment Environment { get; set; } = new();
 
-        [JsonPropertyName("external_id")]
-        public string? ExternalId { get; set; }
+    //    [Required]
+    //    [JsonPropertyName("limits")]
+    //    public ServerLimits Limits { get; set; } = new();
 
-        [JsonPropertyName("uuid")]
-        public string Uuid { get; set; } = string.Empty;
+    //    [JsonPropertyName("feature_limits")]
+    //    public FeatureLimits? feature_limits { get; set; }
 
-        [JsonPropertyName("identifier")]
-        public string Identifier { get; set; } = string.Empty;
+    //    [Required]
+    //    [JsonPropertyName("allocation")]
+    //    public AllocationConfig Allocation { get; set; } = new();
 
-        [JsonPropertyName("name")]
-        public string Name { get; set; } = string.Empty;
+    //    [JsonPropertyName("deploy")]
+    //    public DeployConfig? Deploy { get; set; }
 
-        [JsonPropertyName("description")]
-        public string? Description { get; set; }
+    //    [JsonPropertyName("start_on_completion")]
+    //    public bool StartOnCompletion { get; set; } = true;
 
-        [JsonPropertyName("status")]
-        public string? Status { get; set; }
+    //    [JsonPropertyName("skip_scripts")]
+    //    public bool SkipScripts { get; set; } = false;
 
-        [JsonPropertyName("suspended")]
-        public bool Suspended { get; set; }
+    //    [JsonPropertyName("oom_disabled")]
+    //    public bool OomDisabled { get; set; } = false;
+    //}
 
-        [JsonPropertyName("limits")]
-        public ServerLimits Limits { get; set; } = new();
+    //public class Environment{
+    //    public string server_jarfile { get; set; }
+    //    public string build_number { get; set; }
+    //}
+    //public class ServerLimits
+    //{
+    //    [Required]
+    //    [JsonPropertyName("memory")]
+    //    public int Memory { get; set; }
 
-        [JsonPropertyName("feature_limits")]
-        public FeatureLimits FeatureLimits { get; set; } = new();
+    //    [JsonPropertyName("swap")]
+    //    public int Swap { get; set; } = 0;
 
-        [JsonPropertyName("user")]
-        public int User { get; set; }
+    //    [Required]
+    //    [JsonPropertyName("disk")]
+    //    public int Disk { get; set; }
 
-        [JsonPropertyName("node")]
-        public int Node { get; set; }
+    //    [JsonPropertyName("io")]
+    //    public int Io { get; set; } = 500;
 
-        [JsonPropertyName("allocation")]
-        public int Allocation { get; set; }
+    //    [JsonPropertyName("cpu")]
+    //    public int Cpu { get; set; } = 100;
+    //}
 
-        [JsonPropertyName("nest")]
-        public int Nest { get; set; }
+    //public class FeatureLimits
+    //{
+    //    [JsonPropertyName("databases")]
+    //    public int? Databases { get; set; }
 
-        [JsonPropertyName("egg")]
-        public int Egg { get; set; }
+    //    [JsonPropertyName("allocations")]
+    //    public int? Allocations { get; set; }
 
-        [JsonPropertyName("container")]
-        public ContainerInfo Container { get; set; } = new();
+    //    [JsonPropertyName("backups")]
+    //    public int? Backups { get; set; }
+    //}
 
-        [JsonPropertyName("created_at")]
-        public DateTime CreatedAt { get; set; }
+    //public class AllocationConfig
+    //{
+    //    [Required]
+    //    [JsonPropertyName("default")]
+    //    public int Default { get; set; }
+    //}
 
-        [JsonPropertyName("updated_at")]
-        public DateTime UpdatedAt { get; set; }
-    }
+    //public class DeployConfig
+    //{
+    //    [JsonPropertyName("locations")]
+    //    public List<int>? Locations { get; set; }
 
-    public class ContainerInfo
-    {
-        [JsonPropertyName("startup_command")]
-        public string StartupCommand { get; set; } = string.Empty;
+    //    [JsonPropertyName("dedicated_ip")]
+    //    public bool DedicatedIp { get; set; } = false;
 
-        [JsonPropertyName("image")]
-        public string Image { get; set; } = string.Empty;
+    //    [JsonPropertyName("port_range")]
+    //    public List<string>? PortRange { get; set; }
+    //}
 
-        [JsonPropertyName("installed")]
-        public bool Installed { get; set; }
+    //// Response model for created server
+    //public class ServerResponse
+    //{
+    //    [JsonPropertyName("object")]
+    //    public string Object { get; set; } = string.Empty;
 
-        [JsonPropertyName("environment")]
-        public Dictionary<string, string> Environment { get; set; } = new();
-    }
+    //    [JsonPropertyName("attributes")]
+    //    public ServerAttributes Attributes { get; set; } = new();
+    //}
+
+    //public class ServerAttributes
+    //{
+    //    [JsonPropertyName("id")]
+    //    public int Id { get; set; }
+
+    //    [JsonPropertyName("external_id")]
+    //    public string? ExternalId { get; set; }
+
+    //    [JsonPropertyName("uuid")]
+    //    public string Uuid { get; set; } = string.Empty;
+
+    //    [JsonPropertyName("identifier")]
+    //    public string Identifier { get; set; } = string.Empty;
+
+    //    [JsonPropertyName("name")]
+    //    public string Name { get; set; } = string.Empty;
+
+    //    [JsonPropertyName("description")]
+    //    public string? Description { get; set; }
+
+    //    [JsonPropertyName("status")]
+    //    public string? Status { get; set; }
+
+    //    [JsonPropertyName("suspended")]
+    //    public bool Suspended { get; set; }
+
+    //    [JsonPropertyName("limits")]
+    //    public ServerLimits Limits { get; set; } = new();
+
+    //    [JsonPropertyName("feature_limits")]
+    //    public FeatureLimits FeatureLimits { get; set; } = new();
+
+    //    [JsonPropertyName("user")]
+    //    public int User { get; set; }
+
+    //    [JsonPropertyName("node")]
+    //    public int Node { get; set; }
+
+    //    [JsonPropertyName("allocation")]
+    //    public int Allocation { get; set; }
+
+    //    [JsonPropertyName("nest")]
+    //    public int Nest { get; set; }
+
+    //    [JsonPropertyName("egg")]
+    //    public int Egg { get; set; }
+
+    //    [JsonPropertyName("container")]
+    //    public ContainerInfo Container { get; set; } = new();
+
+    //    [JsonPropertyName("created_at")]
+    //    public DateTime CreatedAt { get; set; }
+
+    //    [JsonPropertyName("updated_at")]
+    //    public DateTime UpdatedAt { get; set; }
+    //}
+
+    //public class ContainerInfo
+    //{
+    //    [JsonPropertyName("startup_command")]
+    //    public string StartupCommand { get; set; } = string.Empty;
+
+    //    [JsonPropertyName("image")]
+    //    public string Image { get; set; } = string.Empty;
+
+    //    [JsonPropertyName("installed")]
+    //    public bool Installed { get; set; }
+
+    //    [JsonPropertyName("environment")]
+    //    public Dictionary<string, string> Environment { get; set; } = new();
+    //}
 
 }
